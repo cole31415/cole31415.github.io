965945c1494d11d503d71482621690019bf15aff
zhouzhibin <1056075535@qq.com>
2020-10-30 09:53:36 +0800

    update: 注释主题打印信息

--- js/utils.js
+++ js/utils.js
@@ -207,27 +207,27 @@ Stun.utils = Stun.$u = {
     }
     return init(function () {})
   },
-  showThemeInConsole: function () {
-    var stunInfo = '主题不错？⭐star 支持一下 ->'
-    var stunURL = 'https://github.com/liuyib/hexo-theme-stun'
-    var stunNameStr =
-      '\n\n      ___          ___            ___            ___      \n     /\\  \\        /\\  \\          /\\__\\          /\\__\\     \n    /::\\  \\       \\:\\  \\        /:/  /         /::|  |    \n   /:/\\ \\  \\       \\:\\  \\      /:/  /         /:|:|  |    \n  _\\:\\ \\ \\  \\      /::\\  \\    /:/  /  ___    /:/|:|  |__  \n /\\ \\:\\ \\ \\__\\    /:/\\:\\__\\  /:/__/  /\\__\\  /:/ |:| /\\__\\ \n \\:\\ \\:\\ \\/__/   /:/  \\/__/  \\:\\  \\ /:/  /  \\/__|:|/:/  / \n  \\:\\ \\:\\__\\    /:/  /        \\:\\  /:/  /       |:/:/  /  \n   \\:\\/:/  /    \\/__/          \\:\\/:/  /        |::/  /   \n    \\::/  /                     \\::/  /         /:/  /    \n     \\/__/                       \\/__/          \\/__/     \n                                                          \n'
-    var stunInfoStyle =
-      'background-color: #49b1f5; color: #fff; padding: 8px; font-size: 14px;'
-    var stunURLStyle =
-      'background-color: #ffbca2; padding: 8px; font-size: 14px;'
-    var stunNameStyle = 'background-color: #eaf8ff;'
+  // showThemeInConsole: function () {
+  //   var stunInfo = '主题不错？⭐star 支持一下 ->'
+  //   var stunURL = 'https://github.com/liuyib/hexo-theme-stun'
+  //   var stunNameStr =
+  //     '\n\n      ___          ___            ___            ___      \n     /\\  \\        /\\  \\          /\\__\\          /\\__\\     \n    /::\\  \\       \\:\\  \\        /:/  /         /::|  |    \n   /:/\\ \\  \\       \\:\\  \\      /:/  /         /:|:|  |    \n  _\\:\\ \\ \\  \\      /::\\  \\    /:/  /  ___    /:/|:|  |__  \n /\\ \\:\\ \\ \\__\\    /:/\\:\\__\\  /:/__/  /\\__\\  /:/ |:| /\\__\\ \n \\:\\ \\:\\ \\/__/   /:/  \\/__/  \\:\\  \\ /:/  /  \\/__|:|/:/  / \n  \\:\\ \\:\\__\\    /:/  /        \\:\\  /:/  /       |:/:/  /  \n   \\:\\/:/  /    \\/__/          \\:\\/:/  /        |::/  /   \n    \\::/  /                     \\::/  /         /:/  /    \n     \\/__/                       \\/__/          \\/__/     \n                                                          \n'
+  //   var stunInfoStyle =
+  //     'background-color: #49b1f5; color: #fff; padding: 8px; font-size: 14px;'
+  //   var stunURLStyle =
+  //     'background-color: #ffbca2; padding: 8px; font-size: 14px;'
+  //   var stunNameStyle = 'background-color: #eaf8ff;'
 
-    console.log(
-      '%c%s%c%s%c%s',
-      stunInfoStyle,
-      stunInfo,
-      stunURLStyle,
-      stunURL,
-      stunNameStyle,
-      stunNameStr
-    )
-  },
+  //   console.log(
+  //     '%c%s%c%s%c%s',
+  //     stunInfoStyle,
+  //     stunInfo,
+  //     stunURLStyle,
+  //     stunURL,
+  //     stunNameStyle,
+  //     stunNameStr
+  //   )
+  // },
   /**
    * Change the event code to keyCode.
    * @param {String} code Event code
